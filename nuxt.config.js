d5cf4f6368249e681bec74321bb2aed015b3673e
ross whitehouse <ross.whitehouse@hotjar.com>
2020-11-16 08:25:17 +0000

    Add generate code.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
+  generate: {
+    fallback: true
+  },
+
   mode: 'spa',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
